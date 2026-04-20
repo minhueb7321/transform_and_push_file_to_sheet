98f2c52cf94b916c8f418d28575c38604c86f595
minh_chim_to <tapsu2001@gmail.com>
2026-04-20 16:02:36 +0700

    Done

--- transform_kpi_activity.py
+++ transform_kpi_activity.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import gspread
-import warnings ; import os; import time ; from datetime import datetime
+import warnings ; import os; import time ; from datetime import datetime; import sys
 warnings.filterwarnings('ignore')
 
 
@@ -34,7 +34,8 @@ def transform_work(path):
     return df_test
 
 def push_data(df ,sheet_id, sheet_name):
-    gc = gspread.service_account(filename = r'F:\Documents\BigqueryJson\sheet_push_data.json')
+    path_to_json =os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    gc = gspread.service_account(filename=path_to_json) 
     sh = gc.open_by_key(sheet_id)
     worksheet = sh.worksheet(sheet_name)
     worksheet.clear()
@@ -42,37 +43,47 @@ def push_data(df ,sheet_id, sheet_name):
 
 if __name__ == '__main__':
     # list base dir
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    path_json = os.path.join(base_dir, 'sheet_push_data.json')
+    try :
+        if getattr(sys, 'frozen', False):
+        # Nếu là file .exe, lấy thư mục chứa file .exe đó
+            base_dir = os.path.dirname(sys.executable)
+        else:
+        # Nếu là file .py bình thường, lấy thư mục chứa file .py
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+        path_json = os.path.join(base_dir, 'sheet_push_data.json')
+        
+        print("Đang khởi tạo môi trường ...")
+        # Initialize env 
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path_json
+
+        # file paths for extract
+        transform_kpi_path = os.path.join(base_dir, 'Báo cáo thực hiện - KPI hoạt động.xlsx')
+        transform_work_path = os.path.join(base_dir, 'Báo cáo chấm công.xlsx')
+
+
+        # sheet id for push
+        sheet_id = '1Wr74ltOP0lDHUXdJVieWSlxq2-aUAGJJS2LMkZqrnvk'
+
+        # sheet name for each data
+        sheet_name_kpi = 'K P I Activity'
+        sheet_name_work = 'W O R K I N G D A Y S'
+
+        # push data KPI
+        push_data(transform_kpi(transform_kpi_path), sheet_id=sheet_id, sheet_name=sheet_name_kpi)
+        print(f"{datetime.now()} || Đẩy data thành công data từ file {transform_kpi_path} vào sheet {sheet_name_kpi}")
+
+        # push data work hours
+        push_data(transform_work(transform_work_path), sheet_id=sheet_id, sheet_name=sheet_name_work)
+        print(f"{datetime.now()} || Đẩy data thành công data từ file {transform_work_path} vào sheet {sheet_name_work}")
+
+
+        print("Thành công !")
+
+        time.sleep(3)
+    except Exception as e:
+        print(f"Đã xảy ra lỗi: {e}")
     
-    print("Đang khởi tạo môi trường ...")
-    # Initialize env 
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path_json
-
-    # file paths for extract
-    transform_kpi_path = os.path.join(base_dir, 'Báo cáo thực hiện - KPI hoạt động.xlsx')
-    transform_work_path = os.path.join(base_dir, 'Báo cáo chấm công.xlsx')
-
-
-    # sheet id for push
-    sheet_id = '1Wr74ltOP0lDHUXdJVieWSlxq2-aUAGJJS2LMkZqrnvk'
-
-    # sheet name for each data
-    sheet_name_kpi = 'K P I Activity'
-    sheet_name_work = 'W O R K I N G D A Y S'
-
-    # push data KPI
-    push_data(transform_kpi(transform_kpi_path), sheet_id=sheet_id, sheet_name=sheet_name_kpi)
-    print(f"{datetime.now()} || Đẩy data thành công data từ file {transform_kpi_path} vào sheet {sheet_name_kpi}")
-
-    # push data work hours
-    push_data(transform_work(transform_work_path), sheet_id=sheet_id, sheet_name=sheet_name_work)
-    print(f"{datetime.now()} || Đẩy data thành công data từ file {transform_work_path} vào sheet {sheet_name_work}")
-
-
-    print("Thành công !")
-
-    time.sleep(3)
-
+    # Dòng này giữ cửa sổ lại
+    input("\nNhấn Enter để thoát...")
 
 
